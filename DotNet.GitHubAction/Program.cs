6ddfc5eff2c575b2bbc439fdfd30886982faa8be
pavel.s@smplt.net <pavel.s@smplt.net>
2023-01-24 12:18:22 +0200

    revert latest github commit

--- DotNet.GitHubAction/Program.cs
+++ DotNet.GitHubAction/Program.cs
@@ -1,4 +1,4 @@
-﻿using IHost host = Host.CreateDefaultBuilder(args)
+using IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureServices((_, services) => services.AddGitHubActionServices())
     .Build();
 
@@ -71,23 +71,9 @@ static async Task StartAnalysisAsync(ActionInputs inputs, IHost host)
     }
 
     // https://docs.github.com/actions/reference/workflow-commands-for-github-actions#setting-an-output-parameter
-    // ::set-output deprecated as mentioned in https://github.blog/changelog/2022-10-11-github-actions-deprecating-save-state-and-set-output-commands/
-    var githubOutputFile = Environment.GetEnvironmentVariable("GITHUB_OUTPUT", EnvironmentVariableTarget.Process);
-    if (!string.IsNullOrWhiteSpace(githubOutputFile))
-    {
-        using (var textWriter = new StreamWriter(githubOutputFile!, true, Encoding.UTF8))
-        {
-            textWriter.WriteLine($"updated-metrics={updatedMetrics}");
-            textWriter.WriteLine($"summary-title={title}");
-            textWriter.WriteLine($"summary-details={summary}");
-        }
-    }
-    else
-    {
-        Console.WriteLine($"::set-output name=updated-metrics::{updatedMetrics}");
-        Console.WriteLine($"::set-output name=summary-title::{title}");
-        Console.WriteLine($"::set-output name=summary-details::{summary}");
-    }
+    Console.WriteLine($"::set-output name=updated-metrics::{updatedMetrics}");
+    Console.WriteLine($"::set-output name=summary-title::{title}");
+    Console.WriteLine($"::set-output name=summary-details::{summary}");
 
     Environment.Exit(0);
 }
@@ -100,7 +86,7 @@ parser.WithNotParsed(
             .CreateLogger("DotNet.GitHubAction.Program")
             .LogError(
                 string.Join(Environment.NewLine, errors.Select(error => error.ToString())));
-
+        
         Environment.Exit(2);
     });
 
